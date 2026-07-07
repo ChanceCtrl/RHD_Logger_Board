11a06167b1adb95b6e652884bad0d389e3f7bdcc
ChanceCtrl <142356358+ChanceCtrl@users.noreply.github.com>
2026-07-06 22:55:26 -0400

    guhu

--- arty-a7/vitis_workspace/arty_eth_platform/microblaze_0/standalone_microblaze_0/bsp/include/DRVLISTConfig.cmake
+++
@@ -1,1 +0,0 @@
-set(DRIVER_LIST bram;common;emaclite;intc;tmrctr;uartlite)
--- arty-a7/vitis_workspace/arty_eth_platform/microblaze_0/standalone_microblaze_0/bsp/libsrc/build_configs/gen_bsp/-.d
+++
@@ -1,2 +0,0 @@
-CXX-DetectStdlib.o: \
- /home/ctrl/Documents/amd/2026.1/Vitis/tps/lnx64/cmake-4.0.1/share/cmake-4.0/Modules/CXX-DetectStdlib.h
